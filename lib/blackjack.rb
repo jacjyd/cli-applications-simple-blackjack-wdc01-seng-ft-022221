c0f71fb370e99ebb1c3ec591cece239f12e15a0f
Diana Liao <dijali@gmail.com>
2021-02-14 22:22:24 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -66,8 +66,7 @@ def runner
   welcome 
   card_total = initial_round
   until card_total >21 do
-      hit?(card_total)
-      card_total
+      card_total = hit?(card_total)
   end
   end_game
 end
